ae6c232f950d60a6276a10b87a5d6e5c24b3f0d1
Peter Shinners <pete@shinners.org>
2025-11-18 18:29:14 -0700

    Cleanup impementation for fresh start

--- src/comp/__init__.py
+++ src/comp/__init__.py
@@ -1,21 +1,12 @@
 """
 Comp Programming Language Implementation
 
-A functional, interpreted programming language designed for general purpose computing.
+A functional, interpreted programming language designed for general purpose
+computing.
 """
 
-__version__ = "0.0.2"
+__version__ = "0.3.0"
 
 
 from ._error import *
-from ._entity import *
 from ._value import *
-from ._function import *
-from ._engine import *
-from ._module import *
-from ._tag import *
-from ._handle import *
-from ._morph import *
-from ._parser import *
-from ._pybridge import *
-from . import builtin, ast, corelib
--- src/comp/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Entry point for running comp as a module (python -m comp)."""
-
-from . import cli
-
-if __name__ == "__main__":
-    cli.main()
